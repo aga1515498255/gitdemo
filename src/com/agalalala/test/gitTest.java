ac37eb076ab814f60743aba37689e225d3f8c079
aga <1515498255@qq.com>
2022-06-06 09:21:59 +0800

    git print 分支修改

--- src/com/agalalala/test/gitTest.java
+++ src/com/agalalala/test/gitTest.java
@@ -1,2 +1,8 @@
 package com.agalalala.test;public class gitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hot-fix 分支的修改2");
+        System.out.println("hot-fix 分支的修改2");
+    }
+
 }

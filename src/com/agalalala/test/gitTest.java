fb58077f59c58fd774c7e11d9f37262718c3f74b
aga <1515498255@qq.com>
2022-06-06 09:34:36 +0800

    master modify

--- src/com/agalalala/test/gitTest.java
+++ src/com/agalalala/test/gitTest.java
@@ -1,2 +1,11 @@
-package com.agalalala.test;public class gitTest {
+package com.agalalala.test;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class gitTest {
+
+    public static void main(String[] args) {
+        System.out.println("master modify");
+        System.out.println("master modify");
+    }
 }
